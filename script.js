d06f6ed9506cc4f28d533d80e7ab39841e93199f
AYUSHBANSAL-hub <72022069+AYUSHBANSAL-hub@users.noreply.github.com>
2023-08-11 08:23:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-//your JS code here. If required.
+let input1 = prompt("Enter the first number:");
+let input2 = prompt("Enter the second number:");
+
+// Write your code here and print the output using alert function
